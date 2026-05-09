07570db1b988709a68e2365f63fe45e9b254f7be
K-S-Teddy <kaushalyasewwandi424@gmail.com>
2026-05-09 13:39:17 +0530

    Add delete complaint route

--- backend/routes/complaintRoutes.js
+++ backend/routes/complaintRoutes.js
@@ -5,7 +5,8 @@ const {
   submitComplaint,
   getMyComplaints,
   getAllComplaints,
-  updateStatus
+  updateStatus,
+  deleteComplaint
 } = require("../controllers/complaintController");
 
 const  protect  = require("../middleware/authMiddleware");
@@ -14,5 +15,6 @@ router.post("/", protect, submitComplaint);
 router.get("/my", protect, getMyComplaints);
 router.get("/all", protect, getAllComplaints);
 router.put("/:id", protect, updateStatus);
+router.delete("/:id", protect, deleteComplaint);
 
 module.exports = router;
