63cdfd56f81d768acb2c4c77228b3214631999dd
K-S-Teddy <kaushalyasewwandi424@gmail.com>
2026-05-09 13:57:28 +0530

    Protect complaint routes for admin acess

--- backend/routes/complaintRoutes.js
+++ backend/routes/complaintRoutes.js
@@ -10,11 +10,13 @@ const {
 } = require("../controllers/complaintController");
 
 const  protect  = require("../middleware/authMiddleware");
+const adminOnly = require("../middleware/adminMiddleware");
 
 router.post("/", protect, submitComplaint);
 router.get("/my", protect, getMyComplaints);
-router.get("/all", protect, getAllComplaints);
-router.put("/:id", protect, updateStatus);
-router.delete("/:id", protect, deleteComplaint);
+
+router.get("/all", protect, adminOnly, getAllComplaints);
+router.put("/:id", protect, adminOnly, updateStatus);
+router.delete("/:id", protect, adminOnly, deleteComplaint);
 
 module.exports = router;
