0972674e813935816147a2fc4de52d4bbb32946e
K-S-Teddy <kaushalyasewwandi424@gmail.com>
2026-05-09 19:42:47 +0530

    update server configuration and middleware setup

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const complaintRoutes = require("./routes/complaintRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/complaints", complaintRoutes);
+app.use("/api/admin", adminRoutes);
 
 
 app.get("/", (req, res) => {
