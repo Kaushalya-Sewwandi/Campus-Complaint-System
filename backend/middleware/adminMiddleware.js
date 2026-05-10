75a17fa3a521a97160e2884c7ed7b2da3c26d709
K-S-Teddy <kaushalyasewwandi424@gmail.com>
2026-05-10 08:59:50 +0530

    Proper checked admin role

--- backend/middleware/adminMiddleware.js
+++ backend/middleware/adminMiddleware.js
@@ -1,9 +1,13 @@
 const adminOnly = (req, res, next) => {
-  if (req.user && req.user.role === "admin") {
-    next();
-  } else {
-    res.status(403).json({ message: "Access denied. Admin only." });
+if(!req.user){
+  return res.status(401).json({message: "Not authenticated"});
+}
+
+  if (req.user.role !== "admin") {
+    return res.status(403).json({ message: "Access denied. Admin only." });
   }
+
+  next();
 };
 
 module.exports = adminOnly;
