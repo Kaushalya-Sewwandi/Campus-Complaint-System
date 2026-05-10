5d03eec237fff9b7325a8117817c82318213adbb
K-S-Teddy <kaushalyasewwandi424@gmail.com>
2026-05-10 08:53:10 +0530

    Fixed issues

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,22 +1,24 @@
 const jwt = require("jsonwebtoken");
 
 const protect = (req, res, next) => {
-  let token = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-  if (token && token.startsWith("Bearer")) {
-    try {
-      token = token.split(" ")[1];
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({message: "No token provided"});
+  }
+  try {
+      const token = authHeader.split(" ")[1];
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       req.user = decoded;
       next();
     } catch (error) {
-      res.status(401).json({ message: "Not authorized" });
-    }
-  } else {
-    res.status(401).json({ message: "No token provided" });
+      console.error(error.message);
+      return res.status(401).json({ message: "Not authorized, token failed" });
+    
   }
 };
+
 
 module.exports = protect;
