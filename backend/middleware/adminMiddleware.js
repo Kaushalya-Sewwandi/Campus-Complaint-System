a58d3485ce2e5dc5f14f7fcc168df421d09e4bee
K-S-Teddy <kaushalyasewwandi424@gmail.com>
2026-05-14 19:00:21 +0530

    Matched with autherMiddleware.js

--- backend/middleware/adminMiddleware.js
+++ backend/middleware/adminMiddleware.js
@@ -1,7 +1,11 @@
 const adminOnly = (req, res, next) => {
-if(!req.user){
-  return res.status(401).json({message: "Not authenticated"});
-}
+  if (!req.user) {
+    return res.status(401).json({ message: "Not authenticated" });
+  }
+
+  if (!req.user.role) {
+    return res.status(403).json({ message: "Role missing in token" });
+  }
 
   if (req.user.role !== "admin") {
     return res.status(403).json({ message: "Access denied. Admin only." });
