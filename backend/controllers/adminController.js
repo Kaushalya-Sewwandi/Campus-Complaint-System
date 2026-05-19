7869df972a9a9623324b9dcd31118284f333eb74
K-S-Teddy <kaushalyasewwandi424@gmail.com>
2026-05-19 20:16:17 +0530

    modify the code populate(student) to populate(studentId)

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,7 +2,7 @@ const Complaint = require("../models/Complaint");
 
 exports.getAllComplaints = async (req, res) => {
   try {
-    const complaints = await Complaint.find().populate("student", "name email");
+    const complaints = await Complaint.find().populate("studentId", "name email");
     res.json(complaints);
   } catch (error) {
     res.status(500).json({ message: error.message });
